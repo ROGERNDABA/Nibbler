6b73c7cdd854015aba44d8570012a0d5633b72bc
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-07-09 10:40:42 +0200

    gg

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ void dl_error(void) {
     std::cerr << "Error : " << dlerror() << std::endl;
 }
 
-int main(int ac, char *av[]) {
-    if (ac == 4) {
-        Nibbler nibbler(av[1], av[2], av[3]);
+int main(int argc, char **argv) {
+    if (argc == 4) {
+        Nibbler nibbler(argv[1], argv[2], argv[3]);
         nibbler.play(-1);
     }
     return 0;
