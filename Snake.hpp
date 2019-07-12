fa2dea905ff17861498da9a039483c0e9869902c
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-07-12 09:52:42 +0200

    Got a lot of shit down

---
+++ Snake.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Snake.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Roger Ndaba <rogerndaba@gmil.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 10:13:56 by Roger Ndaba       #+#    #+#             */
+/*   Updated: 2019/07/05 13:45:58 by Roger Ndaba      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SNAKE_HPP
+#define SNAKE_HPP
+
+#include <dlfcn.h>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// #define WINW 900
+// #define WINH 600
+enum E_KEYS {
+    KEY_UP,
+    KEY_DOWN,
+    KEY_LEFT,
+    KEY_RIGHT
+};
+
+typedef struct SVertex {
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+} TVertex;
+
+typedef struct SnakeS {
+    std::vector<TVertex> *body;
+    std::vector<TVertex> *obstacles;
+    int WINW;
+    int WINH;
+    TVertex food;
+    TVertex bonus;
+    bool *key;
+    int prevKey;
+    bool doExit;
+    float speed;
+    int score;
+    int trackFood;
+    bool valBonus;
+} SnakeT;
+
+class Snake {
+   public:
+    virtual void init() = 0;
+    virtual SnakeT getSnake(void) const = 0;
+    virtual int getEvent(void) const = 0;
+    virtual void updateSnake(SnakeT) = 0;
+    virtual void stop() = 0;
+    virtual ~Snake(){};
+};
+
+#endif  //SNAKE_HPP
