00e8bb5583f00807433770d98d69398e1ca05837
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-07-15 09:35:32 +0200

    allegro still fuckt

--- install.sh
+++ install.sh
@@ -8,48 +8,54 @@ make clean -C SnakeSDL >/dev/null 2>&1
 unset LD_LIBRARY_PATH
 unset DYLD_LIBRARY_PATH
 
-
+echo -e "\e[33mChecking Homebrew...\e[0m"
 which -s brew >/dev/null 2>&1
 if [[ $? != 0 ]] ; then
+    echo -e "\e[31mHomebrew not found...\e[0m"
     echo -e "\e[33mInstalling Homebrew...\e[0m"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
     source ~/.zshrc
 else
+    echo -e "\e[33mUpdating Homebrew...\e[0m"
     brew update
 fi
 
-# hash cmake &> /dev/null
-# if [ $? -eq 1 ]; then
-#     echo -e "\e[33mInstalling CMake...\e[0m"
-#     brew install cmake
-# fi
 
-# echo -e "\e[33mChecking lib folders...\e[0m"
-# if [ -d "$HOME/.brew/Cellar/allegro" ] && [ -d "$HOME/.brew/Cellar/sfml" ] \
-#     && [ -d "$HOME/.brew/Cellar/sdl2" ] && [ -d "$HOME/.brew/Cellar/sdl2_mixer" ] \
-#     && [ -d "$HOME/.brew/Cellar/sdl2_ttf" ]; then
-#     export DYLD_LIBRARY_PATH=$HOME/.brew/Cellar/allegro/*/lib:$HOME/.brew/Cellar/sdl2/*/lib:$HOME/.brew/Cellar/sfml/*/lib
-# else
-#     echo -e "\e[31mLib folders not found...\e[0m"
-#     echo -e "\e[33mInstalling libs. This may take a while...\e[0m"
-#     brew install allegro
-#     brew install sdl2
-#     brew install sfml
-#     export DYLD_LIBRARY_PATH=$HOME/.brew/Cellar/allegro/*/lib:$HOME/.brew/Cellar/sdl2/*/lib:$HOME/.brew/Cellar/sfml/*/lib
-# fi
+echo -e "\e[33mChecking Cmake...\e[0m"
+which -s cmake >/dev/null 2>&1
+if [[ $? != 0 ]] ; then
+    echo -e "\e[33mInstalling CMake...\e[0m"
+    brew install cmake
+fi
 
-# echo -e "\e[33m---Creating project lib folder---\e[0m"
-# mkdir lib
-# echo -e "\e[33m---Making libSnakeAllegro.so---\e[0m"
-# make -C SnakeAllegro >/dev/null 2>&1 && \
-# cp -fr SnakeAllegro/libSnakeAllegro.so lib >/dev/null 2>&1
-# echo -e "\e[33m---Making libSnakeSDL.so---\e[0m"
-# make -C SnakeSDL >/dev/null 2>&1 && \
-# cp -fr SnakeSDL/libSnakeSDL.so lib >/dev/null 2>&1
-# echo -e "\e[33m---Making libSnakeSFML.so---\e[0m"
-# make -C SnakeSFML >/dev/null 2>&1 && \
-# cp -fr SnakeSFML/libSnakeSFML.so lib >/dev/null 2>&1
-# echo -e "\e[33m---Running CMake---\e[0m"
-# cmake .
-# echo -e "\e[33m---Making nibbler---\e[0m"
-# make
+echo -e "\e[33mChecking lib folders...\e[0m"
+if [ -d "$HOME/.brew/opt/allegro" ] && [ -d "$HOME/.brew/opt/sfml" ] \
+    && [ -d "$HOME/.brew/opt/sdl2" ] && [ -d "$HOME/.brew/opt/sdl2_mixer" ] \
+    && [ -d "$HOME/.brew/opt/sdl2_ttf" ]; then
+    export DYLD_LIBRARY_PATH=$HOME/.brew/opt/allegro/lib:$HOME/.brew/opt/sdl2/lib:$HOME/.brew/opt/sdl2_mixer/lib:$HOME/.brew/opt/sdl2_ttf/lib:$HOME/.brew/opt/sfml/lib
+else
+    echo -e "\e[31mLib folders not found...\e[0m"
+    echo -e "\e[33mInstalling libs. This may take a while...\e[0m"
+    brew install allegro
+    brew install sdl2
+    brew install sdl2_mixer
+    brew install sdl2_ttf
+    brew install sfml
+    export DYLD_LIBRARY_PATH=$HOME/.brew/opt/allegro/lib:$HOME/.brew/opt/sdl2/lib:$HOME/.brew/opt/sdl2_mixer/lib:$HOME/.brew/opt/sdl2_ttf/lib:$HOME/.brew/opt/sfml/lib
+fi
+
+echo -e "\e[33m---Creating project lib folder---\e[0m"
+mkdir lib
+echo -e "\e[33m---Making libSnakeAllegro.so---\e[0m"
+make -C SnakeAllegro >/dev/null 2>&1 && \
+cp -fr SnakeAllegro/libSnakeAllegro.so lib >/dev/null 2>&1
+echo -e "\e[33m---Making libSnakeSDL.so---\e[0m"
+make -C SnakeSDL >/dev/null 2>&1 && \
+cp -fr SnakeSDL/libSnakeSDL.so lib >/dev/null 2>&1
+echo -e "\e[33m---Making libSnakeSFML.so---\e[0m"
+make -C SnakeSFML >/dev/null 2>&1 && \
+cp -fr SnakeSFML/libSnakeSFML.so lib >/dev/null 2>&1
+echo -e "\e[33m---Running CMake---\e[0m"
+cmake .
+echo -e "\e[33m---Making nibbler---\e[0m"
+make
