637f619b777f72f130546208dd073e1482e15192
roger NDABA <rmdaba@c4r17s11.wethinkcode.co.za>
2019-07-22 12:15:19 +0200

    cleaning up

--- clean.sh
+++ clean.sh
@@ -2,10 +2,9 @@
 # unset LD_LIBRARY_PATH
 echo -e "\e[31mCleaning up project...\e[0m"
 rm -fr CMakeFiles cmake_install.cmake CMakeCache.txt Makefile nibbler lib
-make clean -C SnakeAllegro >/dev/null 2>&1 &&
 make clean -C SnakeSFML >/dev/null 2>&1 &&
 make clean -C SnakeSDL >/dev/null 2>&1 &&
-make clean -C SnakeFLTK >/dev/null 2>&1 
+make clean -C SnakeFLTK >/dev/null 2>&1
 
 # rm -fr SnakeAllegro/allegro
 # rm -fr SnakeSFML/SFML
